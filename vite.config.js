9cb71cced0ee5fa4f920abc1ef5461f780968e90
alianorafael-ti <portifolio@aliano.com.br>
2026-07-14 13:18:47 -0300

    "fix: adiciona base path para o deploy no github pages"

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  // Adicione a linha abaixo substituindo "regiane-cosmeticos" pelo nome exato do seu repositório no GitHub
+  base: '/regiane-cosmeticos/', 
 })
